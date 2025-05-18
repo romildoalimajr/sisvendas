d7db6cdae938d267d5a0b588c8c320f4c8dac41f
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2025-05-18 20:28:55 -0300

    Implementado os Repositórios com JpaRepository.

---
+++ src/main/java/com/sisvendas/sisvendas/repositories/IClienteRepository.java
@@ -0,0 +1,9 @@
+package com.sisvendas.sisvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sisvendas.sisvendas.models.Cliente;
+
+public interface IClienteRepository extends JpaRepository<Cliente, Long>{
+    
+}
