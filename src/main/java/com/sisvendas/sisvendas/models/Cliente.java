c7747384842b6ad4113dc249aad1d84ed8f63232
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2025-05-18 20:22:40 -0300

    Configurado e estruturado as classes principais.

--- src/main/java/com/sisvendas/sisvendas/models/Cliente.java
+++ src/main/java/com/sisvendas/sisvendas/models/Cliente.java
@@ -1,5 +1,47 @@
 package com.sisvendas.sisvendas.models;
 
-public class Cliente {
-    
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "CLIENTE")
+public class Cliente extends BaseModel{
+    private String nome;
+    private String telefone;
+    private String endereco;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    private List<Pedido> pedido;
+
+    public List<Pedido> getPedido() {
+        return pedido;
+    }
+    public void setPedido(List<Pedido> pedido) {
+        this.pedido = pedido;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
