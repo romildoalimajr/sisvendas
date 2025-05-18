3f6e2d9c36cf14df093c225d4dcb57fad746054f
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2025-05-18 20:52:09 -0300

    Finalizado os services.

---
+++ src/main/java/com/sisvendas/sisvendas/service/ClienteService.java
@@ -0,0 +1,8 @@
+package com.sisvendas.sisvendas.service;
+
+import com.sisvendas.sisvendas.models.Cliente;
+import com.sisvendas.sisvendas.repositories.IClienteRepository;
+
+public class ClienteService extends BaseService<Cliente, IClienteRepository>{
+    
+}
---
+++ src/main/java/com/sisvendas/sisvendas/service/ProdutoService.java
@@ -0,0 +1,8 @@
+package com.sisvendas.sisvendas.service;
+
+import com.sisvendas.sisvendas.models.Produto;
+import com.sisvendas.sisvendas.repositories.IProdutoRepository;
+
+public class ProdutoService extends BaseService<Produto, IProdutoRepository>{
+    
+}
